c37e4278cedb1cad5744b77a196392afc0db011a
YakibchukOstap567 <ostap.yakibchuk02@gmail.com>
2025-06-28 20:09:23 +0200

    urobil som funkcie pre vlozenie hodnoty na koniec zoznamu a pre vypis zoznamu

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,25 +2,55 @@
 #include<stdlib.h>
 
 typedef struct TWN{
-    float data;
+    int data;
     struct TWN *prev;
     struct TWN *next;
-}TWN;
+} TWN;
 
-typedef struct{
+typedef struct CTWL{
     TWN *cur;
     unsigned int size;
-}CTWL;
+} CTWL;
 
-CTWL *ctwl_create_empty(void){
+CTWL *ctwl_create_empty(void) {
 	CTWL* list = (CTWL*)malloc(sizeof(CTWL));
+	if (!list) return NULL;
 	list -> cur = NULL;
 	list -> size = 0;
+	return list;
 }
 
+int ctwl_insert(CTWL* list, int val) {
+	if (!list) return 0;
+	TWN* node = (TWN*)malloc(sizeof(TWN));
+	if (!node) return 0;
+	node -> data = val;
+	
+	if(!list -> cur) {
+	   node -> next = node -> prev = node;
+	   list -> cur = node;
+    } else {
+       TWN* last = list -> cur -> prev;
+       node -> next = list -> cur;
+       node -> prev = last;
+       last -> next = node;
+       list -> cur -> prev = node;
+	}
+	list -> size++;
+	return 1;
+}
 
+void ctwl_print(CTWL *list) {
+	if (!list || !list->cur) return;
+    TWN *current = list->cur;
+	for(unsigned int i = 0; i < list -> size; i++){
+		printf("%d ", current -> data);
+		current = current -> next;
+	}
+	printf("\n");
+}
 
-CTWL *ctwl_sum_demodulation(CTWL *list, unsigned int period){
+CTWL *ctwl_sum_demodulation(CTWL *list, unsigned int period) {
 }
 
 int main() {
@@ -28,6 +58,10 @@ int main() {
     int val_count = sizeof(val) / sizeof(val[0]);
     CTWL* list = ctwl_create_empty();
     for(int i = 0; i < val_count; i++){
-    	
+    	ctwl_insert(list, val[i]);
 	}
+		
+	puts("Povodny zoznam:\n");
+	
+	ctwl_print(list);
 }
