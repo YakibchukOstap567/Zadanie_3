1f2d62f64c5fef52a3369c873095446e430509a4
YakibchukOstap567 <ostap.yakibchuk02@gmail.com>
2025-06-29 17:45:06 +0200

    trochu som zlepsil kod a pridal som nepouzite funkcie infrastruktury

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,5 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <time.h>
+
+#define CTWL_OK 1
+#define CTWL_FAIL 0
 
 typedef struct TWN{
     int data;
@@ -20,7 +24,21 @@ CTWL *ctwl_create_empty(void) {
 	return list;
 }
 
-int ctwl_insert(CTWL* list, int val) {
+void ctwl_destroy(CTWL* list) {
+    if (!list || !list->cur) {
+        free(list);
+        return;
+    }
+    TWN* current = list->cur;
+    for (unsigned int i = 0; i < list->size; i++) {
+        TWN* next = current->next;
+        free(current);
+        current = next;
+    }
+    free(list);
+}
+
+TWN* ctwl_insert_left(CTWL* list, int val) {
 	if (!list) return 0;
 	TWN* node = (TWN*)malloc(sizeof(TWN));
 	if (!node) return 0;
@@ -30,15 +48,51 @@ int ctwl_insert(CTWL* list, int val) {
 	   node -> next = node -> prev = node;
 	   list -> cur = node;
     } else {
-       TWN* last = list -> cur -> prev;
+       TWN* left = list -> cur -> prev;
        node -> next = list -> cur;
-       node -> prev = last;
-       last -> next = node;
+       node -> prev = left;
+       left -> next = node;
        list -> cur -> prev = node;
 	}
 	list -> size++;
-	return 1;
+	return node;
 }
+
+TWN* ctwl_insert_right(CTWL* list, int val) {
+	if (!list) return 0;
+	TWN* node = (TWN*)malloc(sizeof(TWN));
+	if (!node) return 0;
+	node -> data = val;
+	
+	if(!list -> cur) {
+	   node -> next = node -> prev = node;
+	   list -> cur = node;
+    } else {
+       TWN* right = list -> cur -> next;
+       node -> prev = list -> cur;
+       node -> next = right;
+       right -> prev = node;
+       list -> cur -> next = node;
+	}
+	list -> size++;
+	return node;
+}
+
+CTWL* ctwl_create_random(unsigned int size) {
+    CTWL* list = ctwl_create_empty();
+    if (!list) return NULL;
+    srand((unsigned int)time(NULL));
+
+    for (unsigned int i = 0; i < size; i++) {
+        int val = rand() % 100;
+        if (!ctwl_insert_right(list, val)) {
+            ctwl_destroy(list);
+            return NULL;
+        }
+    }
+    return list;
+}
+
 
 void ctwl_print(CTWL *list) {
 	if (!list || !list->cur) return;
@@ -48,6 +102,37 @@ void ctwl_print(CTWL *list) {
 		current = current -> next;
 	}
 	printf("\n");
+}
+
+char ctwl_delete(CTWL* list) {
+    if (!list || !list->cur) return CTWL_FAIL;
+    TWN* targ = list->cur;
+
+    if (list->size == 1) {
+        free(targ);
+        list->cur = NULL;
+    } else {
+        TWN* prev = targ->prev;
+        TWN* next = targ->next;
+        prev->next = next;
+        next->prev = prev;
+        list->cur = next;
+        free(targ);
+    }
+    list->size--;
+    return CTWL_OK;
+}
+
+void ctwl_cur_step_right(CTWL *list) {
+    if (list && list->cur) {
+        list->cur = list->cur->next;
+    }
+}
+
+void ctwl_cur_step_left(CTWL *list) {
+    if (list && list->cur) {
+        list->cur = list->cur->prev;
+    }
 }
 
 CTWL *ctwl_sum_demodulation(CTWL *list, unsigned int period) {
@@ -70,16 +155,8 @@ CTWL *ctwl_sum_demodulation(CTWL *list, unsigned int period) {
 			current = current -> next;
       	}
       	
-      	if (!ctwl_insert(list_2, suma)) {
-            while (list_2 -> size > 0) {
-                TWN* temp = list_2 -> cur;
-                temp -> prev -> next = temp -> next;
-                temp -> next -> prev = temp -> prev;
-                list_2 -> cur = temp -> next;
-                free(temp);
-                list_2 -> size--;
-            }
-            free(list_2);
+      	if (!ctwl_insert_left(list_2, suma)) {
+            ctwl_destroy(list_2);
             return NULL;
         }
 	}
@@ -92,7 +169,7 @@ int main() {
     int val_count = sizeof(val) / sizeof(val[0]);
     CTWL* list = ctwl_create_empty();
     for(int i = 0; i < val_count; i++){
-    	ctwl_insert(list, val[i]);
+    	ctwl_insert_left(list, val[i]);
 	}
 		
 	puts("Povodny zoznam:\n");
@@ -107,6 +184,9 @@ int main() {
 	} else {
 		puts("Chyba: demodulacia zlyhala.\n");
 	}
+	
+	ctwl_destroy(list);
+    ctwl_destroy(list_2);
 	
 	return 0;
 }
